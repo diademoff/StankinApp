dc506cb0628532e4a93fc6e530dd7562b78a2b1c
dmff <diademoff@yandex.ru>
2025-02-19 20:02:22 +0300

    install ui lib

--- StankinApp/Views/MainWindow.axaml.cs
+++ StankinApp/Views/MainWindow.axaml.cs
@@ -1,11 +1,14 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia.Styling;
+using SukiUI;
+using SukiUI.Controls;
 
 namespace StankinApp.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : SukiWindow
 {
     public MainWindow()
     {
+        SukiTheme.GetInstance().ChangeBaseTheme(ThemeVariant.Light);
         InitializeComponent();
     }
 }
