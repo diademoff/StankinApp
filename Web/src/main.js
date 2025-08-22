7eb77d36e7577e0279391523e478b9fd0bf39780
dmff <diademoff@yandex.ru>
2025-08-22 19:16:14 +0500

    edit sw

--- Web/src/main.js
+++ Web/src/main.js
@@ -14,3 +14,11 @@ document.addEventListener('alpine:init', () => {
 Alpine.start();
 
 console.log('Schedule application initialized');
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log("SW registered", reg))
+      .catch(err => console.error("SW failed", err));
+  });
+}
