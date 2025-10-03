a11aceef0f5a9b6294f56d99048438fcdb8c0911
dmff <diademoff@yandex.ru>
2025-10-03 09:36:26 +0300

    deprecated old js project

--- Web/src/config.js
+++
@@ -1,7 +0,0 @@
-const SCHEDULE_CONFIG = {
-    CACHE_PREFIX: 'scheduleCache:v1',
-    CACHE_TTL_MS: 2 * 60 * 60 * 1000, // 2 часа
-    DEFAULT_WEEK_DAY_START: 1, // 0 - воскресенье, 1 - понедельник (Россия)
-};
-
-export default SCHEDULE_CONFIG;
